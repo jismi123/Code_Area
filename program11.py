dd111170366fcc91e70696d983011beb0c558571
Jismi Joseph <73015077+jismi123@users.noreply.github.com>
2023-09-17 15:32:44 +0530

    Update program11.py
    Given an array arr[] of size N. The task is to find the sum of the contiguous subarray within a arr[] with the largest sum.

--- program11.py
+++ program11.py
@@ -8,6 +8,32 @@ def maxarray(arr,size):
             msf=mef
         if mef<0:
             mef=0
-    print(msf)
+    print(msf) #printing just sum
 arr=list(map(int,input().split()))
 maxarray(arr,len(arr))
+"""
+printing sum and the subarray
+from sys import maxsize
+def maxarray(arr,size):
+    mef=0
+    msf=-maxsize-1
+    start=0
+    end=0
+    s=0
+    for i in range(0,size):
+        mef=mef+arr[i]
+        if msf<mef:
+            msf=mef
+            start=s 
+            end=i
+        if mef<0:
+            mef=0
+            s=i+1
+    print(msf)
+    for i in range(start,end+1):
+        print(arr[i],end=" ")
+    
+arr=list(map(int,input().split()))
+maxarray(arr,len(arr))
+
+"""
